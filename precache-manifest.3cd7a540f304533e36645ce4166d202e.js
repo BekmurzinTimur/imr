85b2f7e7288baf3c4dcf1e8379c1315809698aa7
TimurBekmurzin <bekmuriznt@yandex.ru>
2019-08-16 12:30:47 +0500

    Updates

---
+++ precache-manifest.3cd7a540f304533e36645ce4166d202e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84690c00bc0093d870c8e5eaa570907",
+    "url": "/imr/index.html"
+  },
+  {
+    "revision": "c7d4cbf8e81deacbbcd7",
+    "url": "/imr/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "8285871458eb87393d08",
+    "url": "/imr/static/css/main.89b0b53d.chunk.css"
+  },
+  {
+    "revision": "c7d4cbf8e81deacbbcd7",
+    "url": "/imr/static/js/2.50b3b692.chunk.js"
+  },
+  {
+    "revision": "8285871458eb87393d08",
+    "url": "/imr/static/js/main.5efc7c18.chunk.js"
+  },
+  {
+    "revision": "bab5a371a99300bf5db5",
+    "url": "/imr/static/js/runtime~main.0d770f38.js"
+  }
+]);
